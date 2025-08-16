71ee1e80f77e9614e39a63acbf25bbf44761623f
EL360B <eliasbabaalian@gmail.com>
2025-08-15 23:15:04 -0700

    User input

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from google import genai
 from dotenv import load_dotenv
 
@@ -7,14 +8,20 @@ api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
 
-response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents="Why is the sky blue? Use one paragraph"
-)
+arg = sys.argv[1:]
 
 def main():
- print(response.text)
- print(f"Prompt tokens: " + str(response.usage_metadata.prompt_token_count))
- print(f"Response tokens: " + str(response.usage_metadata.candidates_token_count))   
+    if not arg:
+        print("Error. Please insert a question.")
+        sys.exit(1)
+    
+    question = " ".join(arg)
+
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001', contents=question)
+    print(response.text)
+    print(f"Prompt tokens:" , response.usage_metadata.prompt_token_count)
+    print(f"Response tokens:" , response.usage_metadata.candidates_token_count)   
 
 if __name__ == "__main__":
     main()
