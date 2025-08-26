0d1ee962e47088992d22fc75f8f9f398397920f2
EL360B <eliasbabaalian@gmail.com>
2025-08-26 13:30:30 -0700

    add verbose flag

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 from google import genai
 from dotenv import load_dotenv
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -15,13 +16,30 @@ def main():
         print("Error. Please insert a question.")
         sys.exit(1)
     
-    question = " ".join(arg)
+    if "--verbose" in arg:
+        arg.remove("--verbose")
+        verbose = True
+    else:
+        verbose = False
+
+    user_prompt = " ".join(arg)
+
+    messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+]
 
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents=question)
-    print(response.text)
-    print(f"Prompt tokens:" , response.usage_metadata.prompt_token_count)
-    print(f"Response tokens:" , response.usage_metadata.candidates_token_count)   
+        model='gemini-2.0-flash-001', contents=messages)
+    
+    
+    if verbose:
+        print(f"User prompt:" , user_prompt)
+        print(f"Prompt tokens:" , response.usage_metadata.prompt_token_count)
+        print(f"Response tokens:" , response.usage_metadata.candidates_token_count)
+        print(response.text)
+
+    else:
+        print(response.text)
 
 if __name__ == "__main__":
     main()
