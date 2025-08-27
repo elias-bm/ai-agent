bb054c09cc49c9d46d3ec05a36af3e0aa79423df
EL360B <eliasbabaalian@gmail.com>
2025-08-26 22:59:32 -0700

    work on functions

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,1 +1,8 @@
+import os
+
 def get_files_info(working_directory, directory="."):
+    full_path = os.path.join(working_directory, directory)
+    if directory not in working_directory:
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    if not os.path.isdir(directory):
+        return f'Error: "{directory}" is not a directory'
