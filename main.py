d1c197fb86d6e3fb7887cf4c051518d762169996
elias-bm <eliasbabaalian@gmail.com>
2025-09-01 16:45:09 -0700

    add system prompt

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 from google import genai
 from dotenv import load_dotenv
 from google.genai import types
+from prompts import *
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -29,9 +30,11 @@ def main():
 ]
 
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents=messages)
-    
-    
+        model='gemini-2.0-flash-001',
+        contents=messages,
+        config=types.GenerateContentConfig(system_instruction=system_prompt),
+    )
+
     if verbose:
         print(f"User prompt:" , user_prompt)
         print(f"Prompt tokens:" , response.usage_metadata.prompt_token_count)
