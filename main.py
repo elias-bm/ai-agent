fee953674f102d70813070d5ad4c9490ee3604f0
elias-bm <eliasbabaalian@gmail.com>
2025-09-01 18:08:56 -0700

    function declaration for get_files_info

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from google import genai
 from dotenv import load_dotenv
 from google.genai import types
 from prompts import *
+from functions.get_files_info import schema_get_files_info
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -29,18 +30,23 @@ def main():
     types.Content(role="user", parts=[types.Part(text=user_prompt)]),
 ]
 
+    available_functions = types.Tool(function_declarations=[schema_get_files_info,])
+    config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt)
+
     response = client.models.generate_content(
         model='gemini-2.0-flash-001',
         contents=messages,
-        config=types.GenerateContentConfig(system_instruction=system_prompt),
+        config=config,
     )
 
-    if verbose:
+    if response.function_calls:
+        for function_call_part in response.function_calls:
+            print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+    elif verbose:
         print(f"User prompt:" , user_prompt)
         print(f"Prompt tokens:" , response.usage_metadata.prompt_token_count)
         print(f"Response tokens:" , response.usage_metadata.candidates_token_count)
         print(response.text)
-
     else:
         print(response.text)
 
